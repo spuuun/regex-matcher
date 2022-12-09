fb64cc5d294562f98fe26ee2ccab8cb7dfdf419d
spuuun <matthew.douglas.ross@gmail.com>
2022-12-09 03:18:25 -0500

    fix imports. add func to parse pattern into statements.

--- data.js
+++ data.js
@@ -1001,4 +1001,4 @@ const cases = [
     "2wva9fGsUY"
 ]
 
-export default { cases }
+export default cases 
--- tester.js
+++ tester.js
@@ -1,21 +1,24 @@
-import { cases } from './data'
+import cases from './data.js'
 // const cases = require('./data')
-import { matcher } from './matcher'
+import  matcher from './matcher.js'
 // const matcher = require('./matcher')
 
+const data = "GIDOaEud8V"
 
 const pattern = process.argv[2]
 const expectedMatches = process.argv[3]
 let matches = 0
 
-console.log(cases)
+console.log(typeof(pattern))
 
-// cases.forEach(str => {
-//     // console.log(matcher(str, pattern))
-//     if (matcher(str, pattern)) {
-//         matches++
-//     }
-// })
+cases.forEach(str => {
+    console.log(matcher(str, pattern))
+    if (matcher(str, pattern)) {
+        matches++
+    }
+})
+
+console.log("RESULT AGAINST 1 STRING", matcher(data, pattern))
 
 expectedMatches === matches ? console.log("SUCCESS!!") : console.log("FAILURE")
 console.log("expected matches.........", expectedMatches)
